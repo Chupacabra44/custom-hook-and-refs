6d057b2813346fdece33921d2b92fd65483e3e41
Sanja Daic <sanja.daic86@gmail.com>
2026-01-07 08:51:49 +0100

    refs to select DOM element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useEffect, useRef } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const inputElement = useRef(null);
+
+  const handleSubmit = () => {
+    inputElement.current.style.padding = "200px";
+  };
+
+  // const handleSubmit = () => {
+  //   const element = document.querySelector(".search");
+  //   console.log(element);
+  //   element.style.padding = "50px";
+  //   element.focus();
+  // };
+
+  // useEffect(() => {
+  //   handleSubmit();
+  // }, []);
+
+  // useEffect(() => {
+  //   inputElement.current.focus();
+  //   inputElement.current.style.padding = "50px";
+  // }, []);
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <input type="text" className="search" ref={inputElement} />
+        <button>Click</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
